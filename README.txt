311134fb0520838ed7d0fc6711306df9dd4581d4
Helder Bargao Robalo <bargaorobalo@gmail.com>
2011-09-19 05:58:38 -0300

    projeto finalizado. ler readme.txt

--- README.txt
+++ README.txt
@@ -7,7 +7,13 @@ For those who do not want to pay to have released the free download link.
 
 /*   HOW IT WORKS  */
 You put the URL locked in the form field and click generate URL for download.
-You will receive a link unlocked or wait 10 seconds to be redirected to the link.
+You will receive a link unlocked.
+
+
+/* NEXT UPDATES */
+	- return a message if the form is_empty
+	- return a messagem if the url can't be unlocked.
+	- Integrate with a URL shortener to statistical analysis of the links sent.
 
 Helder Bargão Robalo
 bargaorobalo@gmail.com
--- js/functions.js
+++ js/functions.js
@@ -1,24 +1,26 @@
-$(document).ready(function(){
+Ôªø$(document).ready(function(){
 	
 	$('#formPost').submit(function(event) {
-		event.preventDefault(); //evito que a aÁ„o de submeter o formul·rio seja enviada para n„o dar refresh na pagina
+		event.preventDefault(); //evito que a a√ß√£o de submeter o formul√°rio seja enviada para dar refresh na pagina
 		$('#resultado').empty(); //escondendo o resultado anterior caso sujeito escreva outra url
-		/* Core da soluÁ„o */
+		/* Core da solu√ß√£o */
 		var linkUsuario = $(this).find('input[name="link"]').val(); //pego a URL do formulario
 		linkUsuario = linkUsuario.split('url='); //Quebro ela no parametro do link
-		linkUsuario = linkUsuario[1].split(''); //pego a segunda parte do array que È o link correto invertido
+		linkUsuario = linkUsuario[1].split(''); //pego a segunda parte do array que √© o link correto invertido
 		linkUsuario = linkUsuario.reverse(); //inverto o array
 		linkUsuario = linkUsuario.join(""); //transformo o array em string.
 		
 		/* Resultados */
-		$('#resultado').html("<h2>Clique no link destravado abaixo ou aguarde 5 segundos</h2>");
-		$('#resultado h2').after("<a href='"+linkUsuario+"'>"+linkUsuario+"</a>");
+		$('#resultado').html("<h2>Confira seu link destravado:</h2>");
+		$('#resultado h2').after("<a href='"+linkUsuario+"' target='_blank' title='Abrir√° em nova janela'>"+linkUsuario+"</a>");
 		$('#resultado').show();
 			
-		/*redirecionar automaticamente depois de 5 segundos */
+		/*redirecionar automaticamente depois de 5 segundos
+			- Decidi deixar o usu√°rio escolher se vai querer acessar ou apenas copiar o link.
 		setTimeout(function() {
 			window.location.href = linkUsuario;
 		}, 5000);
+		*/
 	return false;
 	});
 	
