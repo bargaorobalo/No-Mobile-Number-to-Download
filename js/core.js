5959c9ead131b782549aa6319f9856de7ca12ec3
Helder Bargao Robalo <bargaorobalo@gmail.com>
2011-09-19 06:01:33 -0300

    removido arquivo core.js

--- js/core.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-function core(){
-var linkUsuario = $(this).find('input[name="link"]').val(); //pego a URL do formulario
-		linkUsuario = linkUsuario.split('url='); //Quebro ela no parametro do link
-		linkUsuario = linkUsuario[1].split(''); //pego a segunda parte do array que é o link correto invertido
-		linkUsuario = linkUsuario.reverse(); //inverto o array
-		linkUsuario = linkUsuario.join(""); //transformo o array em string.
-}
-});
