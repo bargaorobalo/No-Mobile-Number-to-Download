18ec4b33f96da968ba97baa146f9a667436d8fb0
Helder Bargao Robalo <bargaorobalo@gmail.com>
2011-09-19 07:07:36 -0300

    adicionada especificacao de url padrao

--- js/functions.js
+++ js/functions.js
@@ -3,12 +3,38 @@
 	$('#formPost').submit(function(event) {
 		event.preventDefault(); //evito que a ação de submeter o formulário seja enviada para dar refresh na pagina
 		$('#resultado').empty(); //escondendo o resultado anterior caso sujeito escreva outra url
-		/* Core da solução */
+		/* Core da solução - url padrão, url invertida, base64, hexadecimal */
 		var linkUsuario = $(this).find('input[name="link"]').val(); //pego a URL do formulario
 		linkUsuario = linkUsuario.split('url='); //Quebro ela no parametro do link
 		linkUsuario = linkUsuario[1].split(''); //pego a segunda parte do array que é o link correto invertido
+		//verifica se a URL já está padrão
+		if(linkUsuario[0]+linkUsuario[1]+linkUsuario[2]+linkUsuario[3]+linkUsuario[4]+linkUsuario[5]+linkUsuario[6] == 'http://'){
+			linkUsuario = linkUsuario.join(""); //transformo o array em string.
+		}else{ //URL invertida
 		linkUsuario = linkUsuario.reverse(); //inverto o array
 		linkUsuario = linkUsuario.join(""); //transformo o array em string.
+		}
+		
+		/*
+			filtros:
+			PADRAO
+			INVERTIDA
+			BASE64
+			HEXA
+				-PADRAO
+				-INVERTIDA
+				-BASE64
+					-INVERTIDA
+						-HEXA( N VEZES, MUITO COMPLEXO E DEPENDE DE CADA UM DOS PROGRAMADORES.)
+			
+			Se alguém quiser fazer a decodificação de url Padrao dentro de base64 ou hexa, me manda um email para eu acompanhar o desenvolvimento.
+			bargaorobalo@gmail.com
+					
+		*/
+		
+		
+		
+		
 		
 		/* Resultados */
 		$('#resultado').html("<h2>Confira seu link destravado:</h2>");
@@ -23,6 +49,4 @@
 		*/
 	return false;
 	});
-	
-	
 });
